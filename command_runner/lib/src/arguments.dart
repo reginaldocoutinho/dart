05391621ef78adef31e1e363d7747b7fc378da40
ReginaldoCoutinho <reginaldo_re12@hotmail.com>
2026-03-12 12:08:11 -0300

    Ultima alteração

--- command_runner/lib/src/arguments.dart
+++ command_runner/lib/src/arguments.dart
@@ -8,13 +8,8 @@ enum OptionType { flag, option }
 abstract class Argument {
   String get name;
   String? get help;
-
-  // In the case of flags, the default value is a bool.
-  // In other options and commands, the default value is a String.
-  // NB: flags are just Option objects that don't take arguments
   Object? get defaultValue;
   String? get valueHelp;
-
   String get usage;
 }
 
